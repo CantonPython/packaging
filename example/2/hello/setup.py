48ae64649b88c0605a4f9a4f7887d3872c7b4533
Michael Meffie <mmeffie@sinenomine.net>
2018-09-14 21:27:14 -0400

    example 2

---
+++ example/2/hello/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hello',
+    version='1.0.0',
+    author='Michael Meffie',
+    author_email='mike@meffie.org',
+    description='A tiny example package',
+    url='https://github.com/CantonPython/packaging',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+    ],
+)
